043bc9eea2ddaff5e1d59ad2b6c1df0fb87098d9
Ranjeet kumar <ranjeetgupta156@gmail.com>
2021-05-22 20:36:20 +0530

    enable cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require('cors')
 const API = require('./index');
 
 const BSEAPI = API.BSE;
 const NSEAPI = API.NSE;
 
 const app = express();
+app.use(cors())
 const port =process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
